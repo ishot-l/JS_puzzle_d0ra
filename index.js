18561a4860487ccce33db4501f03981ef9e68899
Toshihiko Doi <36196747+ishot-l@users.noreply.github.com>
2019-10-19 01:35:22 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -79,6 +79,14 @@ function drawing() {
     }    
 }
 
+function PuzzleProcessing(){
+    // パズルを動かしてターンを進める
+    console.log("パズル処理");
+
+    // おわったら
+    state = 0;
+}
+
 
 let board = new Array(BOARD_HEIGHT);
 for(let i=0; i<BOARD_HEIGHT; i++){
@@ -88,19 +96,105 @@ for(let i=0; i<BOARD_HEIGHT; i++){
     }
 }
 
+
+// パズル画面
 let BottomWindow = document.getElementById('BottomWindow');
 let Puzzle = document.createElement('div');
 Puzzle.classList.add('Puzzle');
 BottomWindow.appendChild(Puzzle);
 
-
+// パズル画面を作成したのでドロップ追加
 drawing();
+
+// 音声の初期化
+let DropSound = document.getElementById('sound-drop');
+
+function sound_drop()
+{
+    DropSound.currentTime = 0;
+
+    const playPromise = DropSound.play();
+    if (playPromise !== null){
+        playPromise.catch(() => { DropSound.play(); });
+    }
+}
+
+
 
 // rootでタッチ捕捉イベント
 let root = document.querySelector('.root');
 let puzzlePosition = Puzzle.getBoundingClientRect();
 
+// パズル中についてくるドロップ
+let HandingDrop = document.createElement('div');
+HandingDrop.classList.add('Jewel');
+HandingDrop.setAttribute('id', 'HandingJewel');
+root.appendChild(HandingDrop);
+
+let handing_info = HandingDrop.getBoundingClientRect();
+let HandingDropWidth = (puzzlePosition.right - puzzlePosition.left) * 0.16;
+let HandingDropHeight = (puzzlePosition.bottom - puzzlePosition.top) * 0.19;
+
+
+// 持っているドロップのタイプ
+HandingDropType = 0;
+
+// 
+function appearHandingDrop(){
+    HandingDrop.style.display = 'block';
+    HandingDrop.classList.add(JEWEL_CLASS_NAME[HandingDropType]);
+}
+
+function disappearHandingDrop(){
+    HandingDrop.style.display = 'none';
+    HandingDrop.classList.remove(JEWEL_CLASS_NAME[HandingDropType]);
+}
+
+// 持っているドロップ移動させる関数
+function MoveHandingDrop(event){
+    // 半分左上に移動させる必要があるな
+    HandingDrop.style.top = Math.round(event.changedTouches[0].pageY - (HandingDropWidth / 2)).toString() + 'px';
+    HandingDrop.style.left = Math.round(event.changedTouches[0].pageX - (HandingDropHeight / 2)).toString() + 'px';
+
+}
+
+// 設定とか
+
+/*
+0:パズル待ち
+1:パズル中
+2:パズル後処理（消える・落ちコン）
+3:ターン移動処理（攻撃とか）
+-1: モーダル等イベント待機中
+*/
+let state = 0;
+
+// 音声流すかどうか
+let IsSoundEffect = false;
+
+
+// 音声切り替えボタン
+let SoundButton = document.querySelector('.SoundButton');
+
+
+SoundButton.addEventListener('touchstart', function(event) {
+    if(!IsSoundEffect){
+        IsSoundEffect = true;
+        SoundButton.classList.remove('fa-volume-mute');
+        SoundButton.classList.add('fa-volume-up');
+    }else{
+        IsSoundEffect = false;
+        SoundButton.classList.remove('fa-volume-up');
+        SoundButton.classList.add('fa-volume-mute');
+    }
+}, false);
+
+
 root.addEventListener('touchstart', function(event) {
+    if(state !== 0){
+        return false;
+    }
+    
     let touchX = event.changedTouches[0].pageX;
     let touchY = event.changedTouches[0].pageY;
     selectMoved = false;
@@ -108,7 +202,10 @@ root.addEventListener('touchstart', function(event) {
     if(!(puzzlePosition.left <= touchX && puzzlePosition.right >= touchX && puzzlePosition.top <= touchY && puzzlePosition.bottom >= touchY)){
         return false;
     }
-    console.log("touched!");
+
+    // 正しい範囲でタップしているなら
+    state = 1;
+
     let {touchXarea, touchYarea} = getTouchedArea(event, puzzlePosition);
     
     selectedX = touchXarea;
@@ -116,15 +213,30 @@ root.addEventListener('touchstart', function(event) {
 
     updateSelectedJewel();
 
+    // HandingDropTypeの指定をわすれずに
+    HandingDropType = board[selectedY][selectedX];
+
+    appearHandingDrop();
+    MoveHandingDrop(event);
+
 }, false);
 
 root.addEventListener('touchmove', function(event) {
     event.preventDefault(); // タッチによる画面スクロールを止める
+    if(state !== 1){
+        return false;
+    }
+
+
     let {touchXarea, touchYarea} = getTouchedArea(event, puzzlePosition);
+
+    MoveHandingDrop(event);
 
     if(touchXarea == selectedX && touchYarea == selectedY) return false;
 
-    console.log("move!");
+    // ドロップが移動した
+    if(IsSoundEffect) sound_drop();
+
     selectMoved = true;
 
     swapJewel(touchXarea, touchYarea, selectedX, selectedY);
@@ -138,13 +250,19 @@ root.addEventListener('touchmove', function(event) {
 
 root.addEventListener('touchend', function(event) {
     // 消したりいろんな処理
-    console.log("end!");
     selectedX = -1;
     selectedY = -1;
     updateSelectedJewel();
+    disappearHandingDrop();
 
     if(selectMoved){
-        console.log("kesusyori");
+        state = 2;
+        PuzzleProcessing();
+        
+    }else{
+        state = 0;
     }
 
 }, false);
+
+
