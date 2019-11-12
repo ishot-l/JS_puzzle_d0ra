60b81d1271d64cdf071f561ef5e416b9bd13813e
Toshihiko Doi <36196747+ishot-l@users.noreply.github.com>
2019-11-12 12:07:35 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ let selectedX = -1;
 let selectedY = -1;
 let selectMoved = false;
 
-
 function getRandom(min, max){
     let random = Math.floor(Math.random() * (max + 1 - min)) + min;
     return random;
@@ -79,14 +78,229 @@ function drawing() {
     }    
 }
 
-function PuzzleProcessing(){
-    // パズルを動かしてターンを進める
-    console.log("パズル処理");
-
-    // おわったら
-    state = 0;
+// パズル処理関連
+let ischecked = new Array(BOARD_HEIGHT);
+for(let i=0; i<BOARD_HEIGHT; i++){
+    ischecked[i] = new Array(BOARD_WIDTH);
+    for(let j=0; j < BOARD_WIDTH; j++){
+        ischecked[i][j] = false;
+    }
 }
 
+let contiH = new Array(BOARD_HEIGHT);
+for(let i=0; i<BOARD_HEIGHT; i++){
+    contiH[i] = new Array(BOARD_WIDTH);
+    for(let j=0; j < BOARD_WIDTH; j++){
+        contiH[i][j] = 0;
+    }
+}
+
+let contiV = new Array(BOARD_HEIGHT);
+for(let i=0; i<BOARD_HEIGHT; i++){
+    contiV[i] = new Array(BOARD_WIDTH);
+    for(let j=0; j < BOARD_WIDTH; j++){
+        contiV[i][j] = 0;
+    }
+}
+
+function ResetCheckedFlag(){
+    for(let i=0; i<BOARD_HEIGHT; i++){
+        ischecked[i] = new Array(BOARD_WIDTH);
+        for(let j=0; j < BOARD_WIDTH; j++){
+            ischecked[i][j] = false;
+        }
+    }
+}
+
+function CountConnectedDrop(x, y, type, count, is_horiz){
+    /*
+    ・範囲外の場合
+    ・チェック済みの場合
+    ・タイプが異なる場合
+    ・タイプがNoneな場合（すでに消されている）
+    は数えない
+    */
+    if(x < 0 || y < 0 || x >= BOARD_WIDTH || y >= BOARD_HEIGHT ||
+        ischecked[y][x] === true || board[y][x] !== type || type === 0){
+        return count;
+    }
+
+    count += 1;
+    ischecked[y][x] = true;
+
+    if(is_horiz){
+        count = CountConnectedDrop(x-1, y, type, count, is_horiz);
+        count = CountConnectedDrop(x+1, y, type, count, is_horiz);
+    }else{
+        count = CountConnectedDrop(x, y-1, type, count, is_horiz);
+        count = CountConnectedDrop(x, y+1, type, count, is_horiz);
+    } 
+
+    return count;
+}
+
+function RemoveDrop(x, y, type){
+    if(x < 0 || y < 0 || x >= BOARD_WIDTH || y >= BOARD_HEIGHT ||
+        board[y][x] !== type || type === 0){
+        return false;
+    }
+
+    document.getElementById(`${y}_${x}`).classList.remove(JEWEL_CLASS_NAME[board[y][x]]);
+    board[y][x] = 0;
+    document.getElementById(`${y}_${x}`).classList.add(JEWEL_CLASS_NAME[board[y][x]]);
+    
+    if(contiH[y][x] >= 3){
+        RemoveDrop(x-1, y, type);
+        RemoveDrop(x+1, y, type);
+    }
+
+    if(contiV[y][x] >= 3){
+        RemoveDrop(x, y-1, type);
+        RemoveDrop(x, y+1, type);
+    }
+
+    return true;
+}
+
+
+// コンボ回数
+let combocount = 1;
+
+function PuzzleProcessing(){
+    // boardから消えるところを抜き出す
+    ResetCheckedFlag();
+
+    let removedrops = [];
+
+    // 横を見ていく
+    for(let i=0; i<BOARD_HEIGHT; i++){
+        for(let j=0; j < BOARD_WIDTH; j++){
+            count = CountConnectedDrop(j, i, board[i][j], 0, true);
+
+            if(count === 0){
+                contiH[i][j] = contiH[i][j-1];
+            }else{
+                contiH[i][j] = count;
+            }
+
+        }
+    }
+
+    ResetCheckedFlag();
+
+    // 縦を見ていく
+    for(let i=0; i<BOARD_HEIGHT; i++){
+        for(let j=0; j < BOARD_WIDTH; j++){
+            count = CountConnectedDrop(j, i, board[i][j], 0, false);
+            if(count === 0){
+                contiV[i][j] = contiV[i-1][j];
+            }else{
+                contiV[i][j] = count;
+            }
+        }
+    }
+
+    //ここエラーおきないか？
+    dropcheck = DropCheck();
+
+    DropCheckLoop();
+
+}
+
+function DropCheckLoop() {
+    console.log("DropCheckLoop");
+    let isCleared = false;
+
+    dropcheckloop = setInterval(()=>{
+        ret = dropcheck.next(combocount);
+        if(ret.done){
+            if(isCleared){
+                DropFallLoop();
+            }else{
+                // 余分に足しちゃった分を消す
+                combocount--;
+                NextTurnProcess();
+                // DeleteComboCounter();
+                console.log("次のターン");
+            }
+            clearInterval(dropcheckloop);
+        }else{
+            isCleared = true;
+            combocount++;
+        }
+    }, 500);
+}
+
+function *DropCheck() {
+    // 消す処理
+    for(let i=0; i<BOARD_HEIGHT; i++){
+        for(let j=0; j < BOARD_WIDTH; j++){
+            if (contiH[i][j] >= 3 || contiV[i][j] >= 3) {
+                if(RemoveDrop(j, i, board[i][j], true)){
+                    AddComboCounter(i, j);
+                    if(IsSoundEffect) sound_combo();
+                    yield;
+                }
+            }
+        }
+    }
+}
+
+function DropFallLoop() {
+    console.log("DropFallLoop");
+    dropfallloop = setInterval(()=>{
+        if(!DropFall()){
+            console.log("ドロップ落ち切った");
+            PuzzleProcessing();
+            clearInterval(dropfallloop);
+        }
+    }, 100);
+}
+
+function DropFall() {
+    let dropped = false;
+
+    for(j=0; j<BOARD_WIDTH; j++){
+        fallthiscol = false;
+        for(i = BOARD_HEIGHT-1; i>=0; i--){
+            if(fallthiscol){
+                swapJewel(j, i, j, i+1);
+            }
+            if(board[i][j] == 0){
+                fallthiscol = true;
+            }
+            if(i == 0 && fallthiscol){
+                document.getElementById(`${i}_${j}`).classList.remove(JEWEL_CLASS_NAME[board[i][j]]);
+                board[i][j] = getRandom(1, 6);
+                document.getElementById(`${i}_${j}`).classList.add(JEWEL_CLASS_NAME[board[i][j]]);
+            }
+        }
+        if(fallthiscol) dropped = true;
+    }
+    return dropped;
+}
+
+
+
+// 何コンボか表示する
+function AddComboCounter(i, j){
+    let dropposition = document.getElementById(`${i}_${j}`).getBoundingClientRect();
+
+    let counter = document.createElement('div');
+    counter.classList.add('ComboCounter');
+    counter.textContent = `${combocount}コンボ`;
+    
+    counter.style.top = Math.round(dropposition.top + 10).toString() + 'px';
+    counter.style.left = Math.round(dropposition.left + 20).toString() + 'px';
+    root.appendChild(counter);
+}
+
+function DeleteComboCounter(){
+    const elements = document.getElementsByClassName('ComboCounter');
+    while (elements.length) {
+        elements.item(0).remove()
+    }
+}
 
 let board = new Array(BOARD_HEIGHT);
 for(let i=0; i<BOARD_HEIGHT; i++){
@@ -94,6 +308,23 @@ for(let i=0; i<BOARD_HEIGHT; i++){
     for(let j=0; j < BOARD_WIDTH; j++){
         board[i][j] = getRandom(1, 6);
     }
+}
+
+// 次のターンへ進む処理
+function NextTurnProcess(){
+    // 敵の攻撃とか
+    state = 3;
+
+    root.dispatchEvent(new Event('TurnChange'));
+
+    // コンボカウントをリセット
+    combocount = 1;
+
+    DeleteComboCounter();
+
+    // おわったら
+    state = 0;
+    
 }
 
 
@@ -108,6 +339,7 @@ drawing();
 
 // 音声の初期化
 let DropSound = document.getElementById('sound-drop');
+let ComboSound = document.getElementById('sound-combo');
 
 function sound_drop()
 {
@@ -116,6 +348,16 @@ function sound_drop()
     const playPromise = DropSound.play();
     if (playPromise !== null){
         playPromise.catch(() => { DropSound.play(); });
+    }
+}
+
+function sound_combo()
+{
+    ComboSound.currentTime = 0;
+
+    const playPromise = ComboSound.play();
+    if (playPromise !== null){
+        playPromise.catch(() => { ComboSound.play(); });
     }
 }
 
@@ -189,6 +431,12 @@ SoundButton.addEventListener('touchstart', function(event) {
     }
 }, false);
 
+// はじめからボタン
+let ReloadButton = document.querySelector('.ReloadButton')
+ReloadButton.addEventListener('touchstart', ()=>{
+    window.location.reload();
+}, false);
+
 
 root.addEventListener('touchstart', function(event) {
     if(state !== 0){
@@ -227,7 +475,6 @@ root.addEventListener('touchmove', function(event) {
         return false;
     }
 
-
     let {touchXarea, touchYarea} = getTouchedArea(event, puzzlePosition);
 
     MoveHandingDrop(event);
@@ -249,6 +496,9 @@ root.addEventListener('touchmove', function(event) {
 }, false);
 
 root.addEventListener('touchend', function(event) {
+    if(state == -1){
+        return;
+    }
     // 消したりいろんな処理
     selectedX = -1;
     selectedY = -1;
@@ -258,10 +508,36 @@ root.addEventListener('touchend', function(event) {
     if(selectMoved){
         state = 2;
         PuzzleProcessing();
-        
     }else{
         state = 0;
     }
+
+    selectMoved = false;
+
+}, false);
+
+let ModalWindow = document.querySelector('.ModalWindow');
+
+let modalstateid;
+
+// モーダルウィンドウ
+function OpenModal(title, description){
+    ModalWindow.querySelector(".title").innerHTML = title;
+    ModalWindow.querySelector(".description").innerHTML = description;
+
+    ModalWindow.style.display = 'block';
+
+    modalstateid = setTimeout(()=>{
+        state = -1;
+    }, 100);    
+}
+
+document.querySelector('.ModalWindow .CloseButton').addEventListener('touchstart', ()=>{
+    let timer1 = setTimeout(()=>{
+        ModalWindow.style.display = 'none';
+        state = 0;
+    }, 200);
+    clearTimeout(modalstateid);
 
 }, false);
 
